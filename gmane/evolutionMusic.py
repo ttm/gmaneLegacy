e65fec6952fa8f9658fb10e96b05eabd8d33f0c2
Renato Fabbri <renato.fabbri@gmail.com>
2015-03-23 07:09:58 -0300

    finished version with two voices

--- gmane/evolutionMusic.py
+++ gmane/evolutionMusic.py
@@ -7,11 +7,13 @@ sy=m.Synth()
 sy.adsrSetup(A=20,D=20,R=10)
 sy.tab=sy.tables.square
 sy2=m.pieces.FourHubsDance()
+sy2.sy=sy
 
 class EvolutionMusic:
     """Make music for appreciation of network evolution"""
     def __init__(self,tdir="./evolution/",label="gmaneID",samplerate=44100):
         self.sy=sy
+        self.sy2=sy2
         self.m=m
         self.samplerate=samplerate
         self.tdir=tdir
@@ -39,38 +41,42 @@ class EvolutionMusic:
         self.degrees_=((n.subtract(self.degrees.T,self.degrees.min(1)) / (self.degrees.max(1)-self.degrees.min(1))).T)
         self.degrees__=(self.degrees_.T+self.off).T
 
-        sy2.setupEngine(self.samplerate,total_time,self.T)
+        self.sy2.setupEngine(self.samplerate,total_time,self.T)
         line2=self.sy2.sonicLine2(
    self.degrees_[-1],self.degrees_[-2])
 
+        sy.adsrSetup(A=5,D=20,S=-20,R=100)
         line4=self.sy2.sonicLine1(
-  self.degrees_[0],[0,3,6,9],ambit=24,rythmic_pattern=[0,0,1,1],f0=440)
+  self.degrees_[0],[0,2,3,4,6,8,9,10],ambit=24,rythmic_pattern=[0,0,1,1],f0=440)
+        sy.adsrSetup(A=5,D=20,S=-10,R=530)
         line3=self.sy2.sonicLine1(
-    self.degrees_[1],[0,4,8],ambit=24,rythmic_pattern=[1],f0=110)
+    self.degrees_[1],[0,2,4,6,8,10],ambit=24,rythmic_pattern=[1],f0=110)
 
+        #UTILS.write(line2+line3[:len(line2)]+line4,"mixY.wav")
+        UTILS.write(line3[:len(line2)]+line4,"mixY.wav")
         # coloca cada um em uma oitava e escala
         # une os vetores de frequencia
         # junta duração
-        self.midi_notes=n.hstack(self.degrees__.T)
+        #self.midi_notes=n.hstack(self.degrees__.T)
 
         # fundo feito pelos perifericos
         # buscar alguma sonoridade ruidosa para os intermediarios 
 
         #self.n_frames=len(self.agents["nm"])
         #self.freqs=co.p2f(110,[[0,7][i%2] for i in range(self.n_frames)])
-        self.freqs=co.p2f(110,self.midi_notes[::4])
-        self.durs=[self.dur_frame]*self.n_frames
-        self.notes=[sy.render(f,d) for f,d in zip(self.freqs,self.durs)]
-        UTILS.write(n.hstack(self.notes),"notes.wav")
+        #self.freqs=co.p2f(110,self.midi_notes[::4])
+        #self.durs=[self.dur_frame]*self.n_frames
+        #self.notes=[sy.render(f,d) for f,d in zip(self.freqs,self.durs)]
+        #UTILS.write(n.hstack(self.notes),"notes.wav")
 
-        self.freqs2=co.p2f(110,self.midi_notes[2::12])
-        self.durs2=[self.dur_frame*3]*(self.n_frames//3)
-        self.notes2=[sy.render(f,d) for f, d in zip(self.freqs2, self.durs2)]
-        UTILS.write(n.hstack(self.notes2),"notes2.wav")
+        #self.freqs2=co.p2f(110,self.midi_notes[2::12])
+        #self.durs2=[self.dur_frame*3]*(self.n_frames//3)
+        #self.notes2=[sy.render(f,d) for f, d in zip(self.freqs2, self.durs2)]
+        #UTILS.write(n.hstack(self.notes2),"notes2.wav")
 
-        mix=n.hstack(self.notes)+n.hstack(self.notes2)
+        #mix=n.hstack(self.notes)+n.hstack(self.notes2)
 
-        UTILS.write(mix,"mix.wav")
+        #UTILS.write(mix,"mix.wav")
     def getOverallMeasures(self):
         filenames=os.listdir(self.tdir)
         filenames_=[i for i in filenames if i.endswith(".pickle")]
--- tests/testEvolutionMusic.py
+++ tests/testEvolutionMusic.py
@@ -1,9 +1,12 @@
-import gmane as g, os
+import gmane as g, mass as m, os
 ENV=os.environ["PATH"]
 import  importlib
 #from IPython.lib.deepreload import reload as dreload
 importlib.reload(g.evolutionMusic)
+importlib.reload(m.pieces.fourHubsDance)
 dreload(g,exclude="pytz")
 os.environ["PATH"]=ENV
+#dreload(m)
+#os.environ["PATH"]=ENV
 
 em=g.EvolutionMusic()
