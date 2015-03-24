06d4392346665b825e6128ad942703265ace35eb
Renato Fabbri <renato.fabbri@gmail.com>
2015-03-23 21:09:50 -0300

    commit to ease development migration

--- gmane/evolutionMusic.py
+++ gmane/evolutionMusic.py
@@ -5,7 +5,7 @@ bt=m.BasicTables()
 co=m.BasicConverter()
 sy=m.Synth()
 sy.adsrSetup(A=20,D=20,R=10)
-sy.tab=sy.tables.square
+#sy.tab=sy.tables.square
 sy2=m.pieces.FourHubsDance()
 sy2.sy=sy
 
@@ -46,8 +46,10 @@ class EvolutionMusic:
    self.degrees_[-1],self.degrees_[-2])
 
         sy.adsrSetup(A=5,D=20,S=-20,R=100)
+        sy.tab=sy.tables.square
         line4=self.sy2.sonicLine1(
   self.degrees_[0],[0,2,3,4,6,8,9,10],ambit=24,rythmic_pattern=[0,0,1,1],f0=440)
+        sy.tab=sy.tables.saw
         sy.adsrSetup(A=5,D=20,S=-10,R=530)
         line3=self.sy2.sonicLine1(
     self.degrees_[1],[0,2,4,6,8,10],ambit=24,rythmic_pattern=[1],f0=110)
