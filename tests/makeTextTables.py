5f46be92d118bcac57cbd7fe08b1480c7eab84d6
Renato Fabbri <renato.fabbri@gmail.com>
2015-10-19 18:35:00 -0200

    adicionado esqueleto minimo

---
+++ tests/makeTextTables.py
@@ -0,0 +1,20 @@
+# file dedicated to making the tables
+# for the main document and the Supporting Information
+# of the artigcle about text differentiation:
+# https://github.com/ttm/artigoTextoNasRedes/ AND
+# http://arxiv.org/abs/1412.7309
+
+# tentar fazer as tabelas focadas nas medidas?
+
+# fazer tabela geral só com CPP
+# incluir mensagens/ano na tabela
+# tirar as datas para a tabela no corpo do artigo
+
+# tentar paralelizar
+
+# fazer tabela de caracteres soh com LAD
+# para o corpo do artigo.
+# renderizar 4 listas
+# avaliar fazer para mais listas para tirar médias e desvios
+
+
